84f0347f2a41747845e51c102221bf341b59a301
Alex Mestemaker <mestemax@mail.uc.edu>
2023-07-23 10:22:50 -0400

    more changes

--- variables.tf
+++ variables.tf
@@ -7,3 +7,4 @@ variable "resource_group_name_prefix" {
   default     = "rg"
   description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
+
