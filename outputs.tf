0aeccb26a7590752cd556d265e3a00a2955c18c7
Alex Mestemaker <mestemax@mail.uc.edu>
2023-07-24 19:50:24 -0400

    adding IP output for faster ssh access

--- outputs.tf
+++ outputs.tf
@@ -2,9 +2,9 @@ output "resource_group_name" {
   value = azurerm_resource_group.rg.name
 }
 
-# output "connect_ip" {
-#   value = data.azurerm_public_ip.connect_ip.ip_address
-# }
+output "connect_ip" {
+  value = data.azurerm_public_ip.connect_ip.ip_address
+}
 
 # output "template" {
 #   value = data.template_file.cloud-init
