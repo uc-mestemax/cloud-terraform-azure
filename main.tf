076505f3a2a2e6d0839bd00d9cbd13d85ad85639
Alex Mestemaker <mestemax@mail.uc.edu>
2023-07-24 21:14:59 -0400

    breaking wireguard config into its own thing

--- main.tf
+++ main.tf
@@ -74,7 +74,7 @@ resource "azurerm_public_ip" "public_ip" {
   name                = "PublicIP"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
 }
 
 # This was one way to define a template file. Not entirely sure how to base64 encode an element like this. 
